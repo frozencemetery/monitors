c46444cb87c8397b8c84863397a111fdce012ee7
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2013-09-13 00:05:20 -0400

    Remove hard-coded path.

--- .mpdmonitor.sh
+++ .mpdmonitor.sh
@@ -3,11 +3,11 @@
 # "##" escaped by mpc to "#"
 
 while true; do
-  nice -n 19 mpc --host /home/frozencemetery/.mpd/socket current -f "<fc=##71BEBE>%artist%</fc> - \"<fc=##FFF796>%title%</fc>\" #[<fc=##CF6171>%album%</fc>#]"
-  mpc --host /home/frozencemetery/.mpd/socket idle
-  if [ -z "$(mpc --host /home/frozencemetery/.mpd/socket playlist)" ]; then
+  nice -n 19 mpc --host ${HOME}/.mpd/socket current -f "<fc=##71BEBE>%artist%</fc> - \"<fc=##FFF796>%title%</fc>\" #[<fc=##CF6171>%album%</fc>#]"
+  mpc --host ${HOME}/.mpd/socket idle
+  if [ -z "$(mpc --host ${HOME}/.mpd/socket playlist)" ]; then
       echo "queueing"
-      cd /home/frozencemetery/Music; find "$(find -maxdepth 1 -mindepth 1 -type d  | shuf | head -n1)" -maxdepth 1 -mindepth 1 -type d | cut -b 1-2 --complement | shuf | head -n1 | mpc --host /home/frozencemetery/.mpd/socket add
-      mpc --host /home/frozencemetery/.mpd/socket play
+      cd ${HOME}/Music; find "$(find -maxdepth 1 -mindepth 1 -type d  | shuf | head -n1)" -maxdepth 1 -mindepth 1 -type d | cut -b 1-2 --complement | shuf | head -n1 | mpc --host ${HOME}/.mpd/socket add
+      mpc --host ${HOME}/.mpd/socket play
   fi
 done

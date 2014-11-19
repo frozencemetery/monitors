aba321bb3f9a20ccd49fa9fc3440a9a4a7e6a8ec
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2014-11-18 20:19:53 -0500

    Merge with .alsamonitor.sh.

--- mpdmonitor.sh
+++ mpdmonitor.sh
@@ -1,13 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-# "##" escaped by mpc to "#"
+export MPD_HOST='/home/frozencemetery/.mpd/socket'
+
+function lpad {
+    word="$1"
+    while [ ${#word} -lt $2 ]; do
+        word="$3$word";
+    done;
+    echo " (${word})";
+}
 
 while true; do
-  nice -n 19 mpc --host ${HOME}/.mpd/socket current -f "<fc=##71BEBE>%artist%</fc> - \"<fc=##FFF796>%title%</fc>\" #[<fc=##CF6171>%album%</fc>#]"
-  mpc --host ${HOME}/.mpd/socket idle
-  if [ -z "$(mpc --host ${HOME}/.mpd/socket playlist)" ]; then
-      echo "queueing"
-      cd ${HOME}/Music; find "$(find -maxdepth 1 -mindepth 1 -type d  | shuf | head -n1)" -maxdepth 1 -mindepth 1 -type d | cut -b 1-2 --complement | shuf | head -n1 | mpc --host ${HOME}/.mpd/socket add
-      mpc --host ${HOME}/.mpd/socket play
-  fi
+    # "##" escaped by mpc to "#"
+    mpc current -f "<fc=##71BEBE>%artist%</fc> - \"<fc=##FFF796>%title%</fc>\" #[<fc=##CF6171>%album%</fc>#]" | tr -d '\n'
+    lpad "$(amixer get Master | tail -n1 | awk '{print $4$6}' | tr -d '[]' | sed -e 's/on//g' -e 's/%off/M/g' -e 's/100/FF/g')" 3 0;
+    mpc idle
+    if [ -z "$(mpc playlist)" ]; then
+        cd /home/frozencemetery/Music; find "$(find -maxdepth 1 -mindepth 1 -type d  | shuf | head -n1)" -maxdepth 1 -mindepth 1 -type d | cut -b 1-2 --complement | shuf | head -n1 | mpc --wait add
+        mpc play
+    fi
 done

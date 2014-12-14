df132a7bfccfbed9e839f98324f5acce0357e84b
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2014-12-13 23:09:31 -0500

    Don't flash on updates.

--- mpdmonitor.sh
+++ mpdmonitor.sh
@@ -14,7 +14,7 @@ while true; do
     # "##" escaped by mpc to "#"
     mpc current -f "<fc=##71BEBE>%artist%</fc> - \"<fc=##FFF796>%title%</fc>\" #[<fc=##CF6171>%album%</fc>#]" | tr -d '\n'
     lpad "$(amixer get Master | tail -n1 | awk '{print $4$6}' | tr -d '[]' | sed -e 's/on//g' -e 's/%off/M/g' -e 's/100/FF/g')" 3 0;
-    mpc idle
+    mpc idle >/dev/null
     if [ -z "$(mpc playlist)" ]; then
         cd /home/frozencemetery/Music; find "$(find -maxdepth 1 -mindepth 1 -type d  | shuf | head -n1)" -maxdepth 1 -mindepth 1 -type d | cut -b 1-2 --complement | shuf | head -n1 | mpc --wait add
         mpc play
